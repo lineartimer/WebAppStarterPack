81f9b7739ffbbdd558414ba09c99c28c07005078
Tamás Kiss <42834892+lineartimecop@users.noreply.github.com>
2025-05-07 11:09:58 +0200

    Reverting to previous version

--- src/Frontend/src/pages/Home/Home.js
+++ src/Frontend/src/pages/Home/Home.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./Home.css";
+import { getData } from "../../services/backend";
+import Table from "../../components/Table/Table";
 
-const Home = () => {
+const Home = ({ token, username }) => {
+    const [data, setData] = useState([]);
+    const [showLogout, setShowLogout] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            var response = await getData(token);
+
+            if (response.ok) {
+                const result = await response.json();
+                setData(result);
+            } else {
+                console.error("Failed to fetch data");
+            }
+        };
+
+        fetchData();
+    }, [token]);
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("username");
+
+        window.location.reload();
+    };
+
     return (
-        <div className="home">
-            <div className="row main">
-                <div className="col-12">
-                    <div className="description">A starter template with a</div>
-                    <div className="description">.Net backend, a React frontend and a</div>
-                    <div className="description">GitHub CI/CD pipeline that deploys to Azure.</div>
-                </div>
+        <div>
+            <div className="header">
+                <a href="#" className="username-link" onClick={(e) => {
+                    e.preventDefault();
+                    setShowLogout(!showLogout);
+                }}>{username}</a>
+                {showLogout && (
+                    <div className="logout-window">
+                        <a href="#" onClick={handleLogout}>
+                            Logout
+                        </a>
+                    </div>
+                )}
             </div>
+            <Table data={data} />
         </div>
     );
 };
